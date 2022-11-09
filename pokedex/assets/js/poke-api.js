ce628125ac6c04e932c83c1a2f6f6f5e7fa8a17a
Lucas Escolástico <lucasescolastico25@gmail.com>
2022-11-09 13:58:15 -0300

    Pokedex primeiro commit

---
+++ pokedex/assets/js/poke-api.js
@@ -0,0 +1,37 @@
+const pokeApi = {}
+
+//Capture the attributes from json to an object
+function convertPokeApiDetailToPokemon (pokeDetail) {  
+    const pokemon = new Pokemon();
+    pokemon.number = pokeDetail.id
+    pokemon.name = pokeDetail.name
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.type = type
+    pokemon.types = types
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+}
+
+//Get the json
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
+}
+
+//Get pokemon details
+pokeApi.getPokemons = (offset = 0, limit = 10) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonDetails) => pokemonDetails)
+}
